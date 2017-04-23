addec46a84f8b0b610b14e0b1687d08d47c8a949
akankshag12 <akankshagarg12@gmail.com>
2017-04-23 11:07:35 -0700

    Updates js (based on feedback)

--- Assignment 5/starter_code/js/script.js
+++ Assignment 5/starter_code/js/script.js
@@ -17,25 +17,30 @@ function changeBackground(){
 	console.log("City: ", city);
 
 	// BASED ON INPUT, CHANGE BACKGROUND 
-	if(city="New York" || "new york" || "NYC"){
+	if(city == "New York" || city == "new york" || city == "NYC"){
 		$("body").attr("class", "nyc");
 		console.log("NY")
 	}
 
-	// else if(city="Los Angeles" || "LAX"){
-	// 	$("body").attr("class", "LAX")
-	// }
+	else if(city == "Los Angeles" || city == "LAX"){
+		$("body").attr("class", "la")
+	}
 
-	// else if(city="Austin" || "ATX"){
-	// 	$("body").attr("class", "austin")
-	// };
+	else if(city == "Austin" || city == "ATX"){
+		$("body").attr("class", "austin")
+	}
 
-	// else (city="Sydney" || "SYD"){
-	// 	$("body").attr("class", "sydney")
-	// }
-	// else if(city="San Francisco" || "SF" || "Bay Area"){
-	// 	$("body").attr("class", "sf")
-	// }
+	else if(city == "Sydney" || city == "SYD"){
+		$("body").attr("class", "sydney")
+	}
+
+	else if(city == "San Francisco" || city == "SF" || city == "Bay Area"){
+		$("body").attr("class", "sf")
+	}
+
+	else{
+		alert("We don't have this city yet!")
+	}
 	}
 })
 
